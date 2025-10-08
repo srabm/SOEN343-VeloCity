35b0cba7e1793937300d3053a77deffb133aff2c
zekaran30 <zekaran30@gmail.com>
2025-10-08 02:47:39 -0400

    backend creation

---
+++ my-app/velocity-backend/src/main/java/com/concordia/veloCity/VeloCityApplication.java
@@ -0,0 +1,13 @@
+package com.concordia.veloCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloCityApplication.class, args);
+	}
+
+}
---
+++ my-app/velocity-backend/src/test/java/com/concordia/veloCity/VeloCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concordia.veloCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
