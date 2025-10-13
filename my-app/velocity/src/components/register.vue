e7853fe7911548b34c9f2ecd347cd59b41b96fc2
Botao Yang <botao5339@gmail.com>
2025-10-12 21:35:00 -0400

    VELO-26: Added register vue template

---
+++ my-app/velocity/src/components/register.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login-container">
+        <h2>Register</h2>
+        <form @submit="">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" id="username" required />
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" required />
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" required />
+            </div>
+            <button type="submit">Register</button>
+        </form>
+        <h2>WELCOME BACK!</h2>
+    </div>
+</template>
