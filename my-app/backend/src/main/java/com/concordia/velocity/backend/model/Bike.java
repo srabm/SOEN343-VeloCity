a742776e6dc7dff612e68b1ce4f46c8c15873f24
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2025-10-15 20:14:09 -0400

    Create base for Bike model

---
+++ my-app/backend/src/main/java/com/concordia/velocity/backend/model/Bike.java
@@ -0,0 +1,17 @@
+package com.concordia.velocity.backend.model;
+
+java.time.LocalDateTime;
+
+abstract class Bike {
+    private String bikeId;
+    private String status;
+    private String type;
+    private LocalDateTime reservationExpiry;
+
+    public User(String bikeId, String status, String type) {
+        this.bikeId = bikeId;
+        this.status = status;
+        this.type = type;
+        this.reservationExpiry = null;
+    }
+}
