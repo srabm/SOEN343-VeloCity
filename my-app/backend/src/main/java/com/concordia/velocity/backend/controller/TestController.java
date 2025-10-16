697eded0672826f09577aba0ad02d16d25ae1089
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2025-10-15 20:08:06 -0400

    Populate User model.

--- my-app/backend/src/main/java/com/concordia/velocity/backend/controller/TestController.java
+++ my-app/backend/src/main/java/com/concordia/velocity/backend/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.concordia.velocity.backend.controllers;
+package com.concordia.velocity.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
