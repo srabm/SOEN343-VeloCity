6d8343dd8549f8c45e53eef82cb29d8b7a4bae1a
jenniferngu <itsjen_ng@hotmail.com>
2025-10-16 15:15:15 -0400

    Pre-Gradle run database set up, couldn't run gradle

--- my-app/backend/src/main/java/com/concordia/velocity/backend/model/Bike.java
+++ my-app/backend/src/main/java/com/concordia/velocity/backend/model/Bike.java
@@ -1,14 +1,20 @@
 package com.concordia.velocity.backend.model;
 
-java.time.LocalDateTime;
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bikes")
 
 abstract class Bike {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String bikeId;
     private String status;
     private String type;
     private LocalDateTime reservationExpiry;
 
-    public User(String bikeId, String status, String type) {
+    public Bike(String bikeId, String status, String type) {
         this.bikeId = bikeId;
         this.status = status;
         this.type = type;
