966078ebb133c9d0370f267509590036b96236a4
Botao Yang <botao5339@gmail.com>
2025-10-18 09:46:44 -0400

    VELO-9: Configured routes and index.js to compartmentalize pages

--- my-app/velocity/src/App.vue
+++ my-app/velocity/src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
-import Register from './components/register.vue'
-import Login from './components/login.vue'
-import PasswordRecovery from './components/passwordRecovery.vue'
 </script>
 
 <template>
-  <Register msg="Vite + Vue" />
-  <Login msg="Vite + Vue" />
-  <PasswordRecovery msg="Vite + Vue" />
+  <router-view />
 </template>
 
 <style scoped>
