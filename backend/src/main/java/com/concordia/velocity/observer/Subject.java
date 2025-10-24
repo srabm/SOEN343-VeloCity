fb68b1ec59a55fdd640f683ee2f42c86fbf84aef
zekaran30 <zekaran30@gmail.com>
2025-10-24 03:39:14 -0400

    VELO-21 add observer

--- backend/src/main/java/com/concordia/velocity/observer/Subject.java
+++ backend/src/main/java/com/concordia/velocity/observer/Subject.java
@@ -3,5 +3,6 @@ package com.concordia.velocity.observer;
 public interface Subject {
     void attach(Observer observer);
     void detach(Observer observer);
+    void reportProblem(Observer observer);
     void notifyObservers();
 }
