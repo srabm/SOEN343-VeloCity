351b40938c9927303e10d3129f00fc321cfe3239
Botao Yang <botao5339@gmail.com>
2025-11-05 21:07:41 -0500

    VELO-40: Added conditional render of logout button and added login button in by detecting if user is connected

--- velocity/src/views/topbar.vue
+++ velocity/src/views/topbar.vue
@@ -1,8 +1,3 @@
-<script setup>
-import Logout from './logout.vue'
-import logo from '../assets/banner.png'
-</script>
-
 <template>
     <header class="bg-white text-black">
         <nav class="grid grid-cols-3 items-stretch h-14 px-4">
@@ -21,13 +16,40 @@ import logo from '../assets/banner.png'
                 </router-link>
             </div>
 
-            <!-- logout -->
+            <!-- Login/Logout -->
             <div class="justify-self-end h-full flex items-center">
-                <Logout />
+                <template v-if="isLoggedIn">
+                    <Logout />
+                </template>
+                <template v-else>
+                    <router-link
+                        to="/login"
+                        class="bg-yellow-300 text-black font-medium px-3 py-1 rounded hover:bg-yellow-400 duration-300"
+                    >
+                        Login
+                    </router-link>
+                </template>
             </div>
         </nav>
     </header>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import Logout from './logout.vue'
+import logo from '../assets/banner.png'
+
+const isLoggedIn = ref(false)
+
+onMounted(() => {
+    const auth = getAuth()
+    onAuthStateChanged(auth, (user) => {
+        isLoggedIn.value = !!user
+    })
+})
+</script>
+
 
 <style scoped>
 
