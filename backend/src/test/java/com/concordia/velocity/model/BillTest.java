40397975b156a1cd45d82b0a2b0df45086b74f02
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2025-11-21 11:15:04 -0500

    Populate Bill total cost unit tests.

--- backend/src/test/java/com/concordia/velocity/model/BillTest.java
+++ backend/src/test/java/com/concordia/velocity/model/BillTest.java
@@ -15,7 +15,7 @@ class BillTest {
 
     @BeforeEach
     void setup() {
-        bill = new Bill("01559657-7fe2-43a5-b74b-3a5b29503217", "5af553e4-98c7-4385-b4e5-7b9346535371", "tQfMbbo4rZPtiVVythjfDb583I93", 13.25, 0, 0);
+        bill = new Bill("01559657-7fe2-43a5-b74b-3a5b29503217", "5af553e4-98c7-4385-b4e5-7b9346535371", "tQfMbbo4rZPtiVVythjfDb583I93", 13.25, 3.00, 0);
     }
 
     @Test
@@ -25,17 +25,24 @@ class BillTest {
         assertEquals("5af553e4-98c7-4385-b4e5-7b9346535371", bill.getTripId());
         assertEquals("tQfMbbo4rZPtiVVythjfDb583I93", bill.getRiderId());
         assertEquals(13.25, bill.getCost());
-        assertEquals(0, bill.getTax());
+        assertEquals(3.00, bill.getTax());
         assertEquals(0, bill.getTotal());
         assertEquals("pending", bill.getStatus());
         assertTrue(currentTime.compareTo(bill.getBillingDate()) >= 0);
     }
 
     @Test
-    void calculateTotalTest() {}
+    void calculateTotalTest() {
+        bill.calculateTotal();
+        assertEquals(16.25, bill.getTotal());
+    }
 
     @Test
-    void calculateTaxTest() {}
+    void calculateTaxTest() {
+        bill.calculateTax(0.14975);
+        assertEquals(1.9841875, bill.getTax());
+        assertEquals(15.2341875, bill.getTotal());
+    }
 
     @AfterEach
     void tearDown() {
