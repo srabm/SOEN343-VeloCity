22089b21bc7a1eb44881f055bda6978ddb822302
Botao Yang <Botao5339@gmail.com>
2025-11-24 10:46:56 -0500

    VELO-64: Updated About Us page and added flex dollars to pricing page

--- velocity/src/views/pricing.vue
+++ velocity/src/views/pricing.vue
@@ -9,6 +9,20 @@
 
             <section class="grid grid-cols-1 md:grid-cols-2 gap-4 max-w-6xl mx-auto text-center px-4">
 
+                <!-- Pay-as-you-go -->
+                <div class="md:col-span-2 rounded-xl p-5 shadow-lg bg-white/50 backdrop-blur-md">
+                    <h2 class="text-xl font-semibold mb-3">Pay-as-you-go Pricing</h2>
+                    <ul class="list-none  space-y-1">
+                        <li>Base (per trip): <strong>$1.11</strong></li>
+                        <li>Regular velo: <strong>$0.22</strong> / min</li>
+                        <li>Electric velo: <strong>$0.33</strong> / min</li>
+                    </ul>
+                    <p class="text-slate-600 mt-3 text-sm">
+                        Default pricing for guests or users without a membership. Charges are calculated when you return
+                        your bike based on trip duration.
+                    </p>
+                </div>
+
                 <!-- Regular Membership -->
                 <div class="rounded-xl p-5 shadow-lg bg-white/50 backdrop-blur-md">
                     <h2 class="text-xl font-semibold mb-3">Regular Membership</h2>
@@ -35,19 +49,16 @@
                     </button>
                 </div>
 
-                <!-- Pay-as-you-go -->
+                <!-- Flex dollars -->
                 <div class="md:col-span-2 rounded-xl p-5 shadow-lg bg-white/50 backdrop-blur-md">
-                    <h2 class="text-xl font-semibold mb-3">Pay-as-you-go Pricing</h2>
-                    <ul class="list-none  space-y-1">
-                        <li>Base (per trip): <strong>$1.11</strong></li>
-                        <li>Regular velo: <strong>$0.22</strong> / min</li>
-                        <li>Electric velo: <strong>$0.33</strong> / min</li>
-                    </ul>
-                    <p class="text-slate-600 mt-3 text-sm">
-                        Default pricing for guests or users without a membership. Charges are calculated when you return
-                        your bike based on trip duration.
+                    <h2 class="text-xl font-semibold mb-3">Flex Dollars</h2>
+                    
+                    <p class="mt-3 px-6">
+                        As an incentive for our users to return their bike at a low capacity station, 2 Flex dollars will be rewarded.
+                        Each Flex dollar will be automatically redeemed for a $0.50 discount on your next ride.
                     </p>
                 </div>
+
             </section>
         </div>
     </div>
